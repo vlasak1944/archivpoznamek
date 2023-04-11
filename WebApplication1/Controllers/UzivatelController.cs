861e3a8ac8bbf4573735610cfbbce0d46a5386f2
Petr Vlasák <vlasak@spstrutnov.cz>
2023-04-11 19:47:24 +0200

    Prihlaseni uzivately WIP

---
+++ WebApplication1/Controllers/UzivatelController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers
+{
+    public class UzivatelController : Controller
+    {
+        [HttpGet]
+        public IActionResult Prihlasit()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Registrovat()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Registrovat(string jmeno, string heslo, string heslo_kontrola)
+            {
+                if(jmeno != null && jmeno.Trim().lenght > 0)
+            }
+
+    }
+}
