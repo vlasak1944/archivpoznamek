82e713e6f9e5a8759c572791541f355049b4caec
Petr Vlasák <vlasak@spstrutnov.cz>
2023-04-12 16:33:31 +0200

    You stupid n

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,5 @@
 using WebApplication1.Data;
 using Microsoft.EntityFrameworkCore;
-using WebApplication1.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
